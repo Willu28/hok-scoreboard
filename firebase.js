938e56427b1edd31981575a707cb3e4082e6f6e2
Willu28 <144191854+Willu28@users.noreply.github.com>
2026-06-29 19:28:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBN9tniz_PWXbufmer92Kg13pf37VWmt7c",
+  authDomain: "hok-scoreboard.firebaseapp.com",
+  projectId: "hok-scoreboard",
+  storageBucket: "hok-scoreboard.firebasestorage.app",
+  messagingSenderId: "842346710648",
+  appId: "1:842346710648:web:b4f4a5a1068793bcb28f8d",
+  measurementId: "G-LNW3QX3TB8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
